9b0c7f97e06c42ca93f65a07c943f6b83609c15a
AbhishekBasak31 <145916390+AbhishekBasak31@users.noreply.github.com>
2026-03-13 16:52:25 +0530

    final

--- src/DB/DB.js
+++ src/DB/DB.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 const DB_Connection = async (db_uri, db_name) => {
     console.log("Connecting to MongoDB...");
+    console.log(`data base name:${db_name}`);
+    console.log(`data base url:${db_uri}`);
+
     try {
         // 🟢 FIX: Use the dbName option instead of `${db_uri}/${db_name}`
         const DB_Initialization = await mongoose.connect(db_uri, {
@@ -12,6 +15,8 @@ const DB_Connection = async (db_uri, db_name) => {
     catch(err){
         console.error("Database connection failed:", err);
         process.exit(1); 
+        process.exit(1); 
     }
 }
 export default DB_Connection;
+
